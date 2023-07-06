31678e5915193156bbefb0ddf8c164193bcd3882
saggemode <saggemode@gmail.com>
2023-07-06 18:43:41 +0100

    lo

---
+++ engine/config.jsx
@@ -0,0 +1,11 @@
+export const nftaddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+export const nftmarketaddress = "0x7Dc2CC126f148543307ebd3418e738Ea9B3CA2D2";
+//EDIT !!!
+let x = 1;
+//change the above everytime you redeploy your contracts
+
+//command for deploying your contracts on hardhat:
+// npx hardhat run --network localhost scripts/deploy.js
+
+//command for starting or restarting your server:
+//npx hardhat node
---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const projectId = "d9px8FgxrnNoTP-YtnNbj8A_rKaj92Hn";
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${projectId}`,
+      accounts: process.env.pkey,
+    },
+    mumbai: {
+      chainId: 80001,
+      url: "https://rpc-mumbai.matic.today",
+      accounts: process.env.pkey,
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+  // solidity: "0.8.9",
+};
